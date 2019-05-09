9e6d446b2efe2e33e053dcd46c71f1dc2d7a4b80
JakeHalopoff <jakehalopoff@u.boisestate.edu>
2019-05-09 12:07:08 -0600

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,9 +1,100 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.math.BigInteger;
 
-public class parser {
+public class Parser {
 
-	parser() {
-		
+	String line = null;
+	BufferedReader in;
+	int position;
+	int seqLength;
+	String sequence;
+
+	public Parser(String file, int i) {
+		FileReader fr = null;
+		seqLength = i;
+		position = 0;
+
+		try {
+			fr = new FileReader(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		in = new BufferedReader(fr);
+		int charPos = 0;
+		sequence = "";
+
+		try {
+			while ((line = in.readLine()) != null) {
+				if (line.startsWith("ORIGIN")) {
+					while (!(line = in.readLine().toLowerCase().trim()).startsWith("//")) {
+						while (charPos < line.length()) {
+							char c = line.charAt(charPos++);
+
+							if (c == 'a') {
+								sequence += "00";
+							}
+							if (c == 't') {
+								sequence += "11";
+							}
+							if (c == 'c') {
+								sequence += "01";
+							}
+							if (c == 'g') {
+								sequence += "10";
+							}
+						}
+						charPos = 0;
+					}
+				}
+
+			}
+
+		} catch (IOException e) {
+			System.err.println("Error in file parsing: characters to bytes");
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Gets the next subsequence of length k in the file string.
+	 * 
+	 * @return - a long with the binary string of data
+	 */
+	public Long nextSubSeq() {
+		String sequenceString = "";
+		long seq;
+
+		for (int i = 0; i < seqLength * 2; i++) {
+			
+			sequenceString += sequence.charAt(position + i);
+			
+		}
+		position += 2;
+
+		seq = Long.parseLong(sequenceString, 2);
+		return seq;
+	}
+	
+	/**
+	 * Returns the size of the sequence. 
+	 * @return
+	 */
+	public int size()
+	{
+		return sequence.length();
+	}
+	
+	public boolean hasNext()
+	{
+		if (position < sequence.length()- seqLength*2)
+		{
+			return true;
+		}
+		return false;
 	}
 	
 	public BigInteger parseBi(String s) {
